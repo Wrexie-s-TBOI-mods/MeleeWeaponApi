ff62d81f8b03a511c5098999770c591adce34e57
Ludovic Fernandez <ludofernandez@msn.com>
2024-11-16 16:37:37 +0100

    chore: Add `main.lua`

---
+++ main.lua
@@ -0,0 +1,9 @@
+-- BL2 Krieg - TBOI: Repentance character mod (c) by Wrexes
+--
+-- BL2 Krieg - TBOI: Repentance character mod is licensed under a
+-- Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+--
+-- You should have received a copy of the license along with this
+-- work. If not, see <https://creativecommons.org/licenses/by-nc-sa/4.0/>.
+
+local mod = RegisterMod("Krieg", 1)
