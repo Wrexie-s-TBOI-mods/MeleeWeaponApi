6bcfc4ed3046a8ec5ddd3c50947fcb8fcb22e56d
Ludovic Fernandez <ludofernandez@msn.com>
2024-11-17 15:18:23 +0100

    chore: Cleanup Buzzaxe implementation
    - Doesn't rely on fake items anymore
    - Charge works *alomst* perfectly: Just needs preventing charge on room clear.
    - Normal VS Beast mode works, including full heal and charge
    
    *For testing purposes, item cooldown and battery pickup are set to
    arbitrary values.

--- README.md
+++ README.md
@@ -88,11 +88,39 @@ Starts with **Buzzaxe** as pocket active item.
 
 ### TODOS
 
-- The whole damn thing
-- Add Hellborn skills
-- Sprites
+- [ ] Charge method
+  - [x] Time
+  - [ ] Prevent room clear charge
+- [ ] Bloodlust
+  - [ ] HUD display
+  - [ ] Stacking
+  - [ ] Over time decay
+  - [ ] Most likely make it a Krieg thing rather than Buzzaxe
+- [ ] Buzzaxe Rampage
+  - [x] Effect duration
+  - [ ] Replace terars with melee
+  - [x] Damage multiplier
+  - [x] Speed multiplier
+  - [x] Release the Beast
+    - [x] Full heal
+    - [x] Item recharge
+    - [x] Damage multiplier
+- [ ] Taste of Blood
+  - [ ] Wafer effect
+  - [ ] Bloodlust stacks
+- [ ] Fuel the Blood
+  - [ ] Explosive damage boost
+- [ ] Blood Trance
+  - [ ] Rampage duration extension
+- [ ] Add Hellborn skills
+- [ ] Sprites/animations
+  - [ ] Buzzaxe
+    - [ ] Weapon sprite
+    - [ ] Release the Beast transformation
+  - [ ] Krieg
 
 ---
 
 ### License
+
 <p xmlns:cc="http://creativecommons.org/ns#" >This work by <a rel="cc:attributionURL dct:creator" property="cc:attributionName" href="https://github.com/SirWrexes">Ludovic F. (aka Sir Wrexes)</a> is licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY-NC-SA 4.0<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1" alt=""></a></p>
--- resources/scripts/Buzzaxe.lua
+++ resources/scripts/Buzzaxe.lua
@@ -6,36 +6,155 @@
 -- You should have received a copy of the license along with this
 -- work. If not, see <https://creativecommons.org/licenses/by-nc-sa/4.0/>.
 
-local BuzzaxeRampage = require "resources.scripts.BuzzaxeRampage"
+---
+--- META DEFINITIONS
+------------------------
+
+---@class BuzzaxeState
+---@field beast         boolean Is the current rampage Beast mode ?
 
 local Buzzaxe = {
     ID = Isaac.GetItemIdByName "Buzzaxe",
+
+    const = {
+        --- Max number of hearts to trigger Relase The Beast.
+        --- 1 unit represents half a heart.
+        RTB_THRESHOLD = 2,
+
+        MULT_SPEED = 1.4,
+
+        MULT_DAMAGE = {
+            --- In Beast mode
+            [true] = 5,
+
+            --- In non Beast mode
+            [false] = 4,
+        },
+
+        MAX_SPEED = 2,
+    },
+
+    ---@type table<integer, BuzzaxeState>
+    state = {},
 }
 
-function Buzzaxe:onUse(item, rng, entity, useFlags, slot, custom)
-    local Player = Isaac.GetPlayer()
-    if Player == nil then return end
+---
+--- STATE MANAGEMENT
+------------------------
 
-    local hearts = Player:GetHearts()
-    local releaseTheBeast = (hearts <= 2)
-    local Effects = Player:GetEffects()
+---@param player EntityPlayer
+function Buzzaxe:getState(player)
+    local id = player:GetPlayerIndex()
 
-    if releaseTheBeast then
-        if Player:CanPickRedHearts() then
-            Player:AddHearts(Player:GetMaxHearts() - hearts)
-        end
+    if Buzzaxe.state[id] == nil then
+        Buzzaxe.state[id] = {
+            beast = false,
+        }
     end
 
-    Effects:AddCollectibleEffect(BuzzaxeRampage.ID)
+    return Buzzaxe.state[id]
+end
+
+---@param player EntityPlayer
+function Buzzaxe:shouldReleaseTheBeast(player)
+    local hearts = player:GetHearts() + player:GetRottenHearts()
+    return hearts <= Buzzaxe.const.RTB_THRESHOLD
+end
+
+---@param player EntityPlayer
+---@return ActiveSlot?
+function Buzzaxe:getSlot(player)
+    for _, slot in pairs(ActiveSlot) do
+        if player:GetActiveItem(slot) == Buzzaxe.ID then
+            return slot
+        end
+    end
+end
+
+---@param player EntityPlayer
+function Buzzaxe:isHoldingBuzzaxe(player)
+    return Buzzaxe:getSlot(player) ~= -1
+end
+
+---@param player EntityPlayer
+function Buzzaxe:isRampaging(player)
+    return player:GetEffects():HasCollectibleEffect(Buzzaxe.ID)
+end
+
+---
+--- MOD CALLBACKS
+------------------------
+
+---@param item CollectibleType
+---@param rng RNG
+---@param player EntityPlayer
+---@param useFlags integer
+---@param slot ActiveSlot
+---@param custom integer
+function Buzzaxe:onPreUseItem(item, rng, player, useFlags, slot, custom)
+    local state = Buzzaxe:getState(player)
+    state.beast = Buzzaxe:shouldReleaseTheBeast(player)
+end
+
+---@param item CollectibleType
+---@param rng RNG
+---@param player EntityPlayer
+---@param useFlags integer
+---@param slot ActiveSlot
+---@param custom integer
+function Buzzaxe:onUseItem(item, rng, player, useFlags, slot, custom)
+    local state = Buzzaxe:getState(player)
+    local hearts = player:GetMaxHearts()
+
+    if state.beast and player:CanPickRedHearts() then
+        player:AddHearts(hearts - Buzzaxe.const.RTB_THRESHOLD)
+    end
 
     return {
-        ShowAnim = false,
+        ShowAnim = true,
         Remove = false,
-        Discharge = not releaseTheBeast,
+        Discharge = true,
     }
 end
 
-function Buzzaxe:preventBatteryPickup(pickup, entity)
+--- Apply speed multiplier during rampage
+---@param player EntityPlayer
+function Buzzaxe:onEvalCacheSpeed(player)
+    if not Buzzaxe:isRampaging(player) then return end
+
+    local speed = player.MoveSpeed * Buzzaxe.const.MULT_SPEED
+    player.MoveSpeed = math.min(speed, Buzzaxe.const.MAX_SPEED)
+end
+
+--- Apply damage multiplier during rampage
+---@param player EntityPlayer
+function Buzzaxe:onEvalCacheDamage(player)
+    if not Buzzaxe:isRampaging(player) then return end
+
+    local state = Buzzaxe:getState(player)
+    player.Damage = player.Damage * Buzzaxe.const.MULT_DAMAGE[state.beast]
+end
+
+--- After a rampage, maybe charge the item if it was beast mode.
+--- Then cleanup state.
+---@param player EntityPlayer
+function Buzzaxe:postRampage(player)
+    if Buzzaxe:isRampaging(player) then
+        return
+    end
+
+    local state = Buzzaxe:getState(player)
+    if not state.beast then return end
+
+    local slot = Buzzaxe:getSlot(player)
+    if slot ~= nil then
+        player:FullCharge(slot, true)
+    end
+
+    state.beast = false
+end
+
+function Buzzaxe:preventBatteryPickup(_pickup, entity)
     local player = entity:ToPlayer()
     if player and player:GetActiveItem() == Buzzaxe.ID then
         return false
@@ -43,8 +162,12 @@ function Buzzaxe:preventBatteryPickup(pickup, entity)
 end
 
 function Buzzaxe.init(mod)
-    mod:AddCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, Buzzaxe.preventBatteryPickup, PickupVariant.PICKUP_LIL_BATTERY)
-    mod:AddCallback(ModCallbacks.MC_USE_ITEM, Buzzaxe.onUse, Buzzaxe.ID)
+    mod:AddCallback(ModCallbacks.MC_PRE_USE_ITEM, Buzzaxe.onPreUseItem, Buzzaxe.ID)
+    mod:AddCallback(ModCallbacks.MC_USE_ITEM, Buzzaxe.onUseItem, Buzzaxe.ID)
+    mod:AddCallback(ModCallbacks.MC_POST_PLAYER_UPDATE, Buzzaxe.postRampage, PlayerVariant.PLAYER)
+    mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, Buzzaxe.onEvalCacheDamage, CacheFlag.CACHE_DAMAGE)
+    mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, Buzzaxe.onEvalCacheSpeed, CacheFlag.CACHE_SPEED)
+    -- mod:AddCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, Buzzaxe.preventBatteryPickup, PickupVariant.PICKUP_LIL_BATTERY)
 end
 
 return Buzzaxe
